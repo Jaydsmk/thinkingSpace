9683f9d3a88a33e9d7df6b52550b404cdecf8f9b
jaydsmk <jaydsmk@gmail.com>
2024-01-27 00:01:44 -0700

    Described thernary operator

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import "./App.css";
 import Test from "./test.js";
 
 function App() {
+  //1. Conditional : if-else, switch ---> ternary opreator
+  //2. Loop: for
+
+  let flag = true;
+  // function CheckFlag(flag) {
+  //   if (flag) {
+  //     return "true";
+  //   } else {
+  //     return "false";
+  //   }
+  // }
+
   return (
     <div>
       <h1>Hello, React!</h1>
+      {/* {CheckFlag(flag)} */}
+      {flag ? "It is True" : "It is False"}
       <Test />
     </div>
   );
