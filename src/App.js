9cb5b748b4274a6e6f2e480c7222997d702770ae
jaydsmk <jaydsmk@gmail.com>
2024-01-27 00:17:21 -0700

    Described Loop

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Test from "./test.js";
 
 function App() {
   //1. Conditional : if-else, switch ---> ternary opreator
-  //2. Loop: for
+  //2. Loop: for ---> map (Should have a unique "key" property)
+  //                  ,and it should use with <div></div>(<></>) to get several values on Loop.
 
   let flag = true;
   // function CheckFlag(flag) {
@@ -14,11 +15,24 @@ function App() {
   //   }
   // }
 
+  let arr = [1, 2, 3];
+
   return (
     <div>
       <h1>Hello, React!</h1>
       {/* {CheckFlag(flag)} */}
       {flag ? "It is True" : "It is False"}
+
+      {arr.map((element, idx) => {
+        return (
+          <div key={idx}>
+            <p>Key value idx = {idx}</p>
+            <p>{element}</p>
+            <p>loop</p>
+          </div>
+        );
+      })}
+
       <Test />
     </div>
   );
