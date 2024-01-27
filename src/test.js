c9e7ecd6e2ba91cda1be79cd33671406e72552d2
jaydsmk <jaydsmk@gmail.com>
2024-01-26 23:48:40 -0700

    Described JSX

--- src/test.js
+++ src/test.js
@@ -6,10 +6,22 @@ import React from "react";
 3. On the contrary, Component should import to use any component
 */
 
+/*
+  JSX:
+1. Using CamelCase (ex: className)
+2. JS: Use with {} when using JS variables or functions.
+3. CSS, Style : use with {{}} + object.
+*/
+
+const temp = 1;
+
 function Test() {
   return (
     <div>
-      <h1>Test Component</h1>
+      <h1 className="" style={{ color: "red", fontSize: "3rem" }}>
+        Test Component <br />
+        {temp}
+      </h1>
     </div>
   );
 }
